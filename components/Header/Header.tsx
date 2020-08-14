a57c7a628d4956365bffd0750cadae4436b41dad
rui-watanabe <wataru610806@gmail.com>
2020-08-14 11:59:09 +0900

    indicate first index.tsx

---
+++ components/Header/Header.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Link from 'next/link'
+
+export const Header: React.FC = () => {
+  return (
+    <Link href="/">
+      <a>home</a>
+    </Link>
+  )
+}
---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Head from 'next/head'
+
+export default function Front() {
+  return (
+    <>
+      <Head>
+        <title>Next App</title>
+      </Head>
+      <main>Hello Next App</main>
+    </>
+  )
+}
